b70ed7654f6330046da5968aeae6b2bdc6808e46
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-05-03 00:12:40 +0530

    add bootstrap

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 const Home = () => {
   return (
-    <div>
+    <Container className='text-center text-primary'>
       HOMEPAGE
-    </div>
+    </Container>
   )
 }
 
