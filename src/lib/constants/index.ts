c67d4769fcf666933b8f6cffd2236a5b103cf8b9
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-05-02 23:33:13 +0530

    add appstate interface

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,1 +1,9 @@
 
+export const DEFAULT_LOCAL_STORAGE_KEY_FOR_APP_STATE = "APP_STATE";
+export const DEFAULT_APP_STATE: AppState = {
+    isUserLoggedIn: false,
+    accessToken: "",
+    refreshToken: "",
+  
+    setAppState: () => {},
+  };
